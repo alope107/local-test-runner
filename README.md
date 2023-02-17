1b989cbb1a6482da1dc1d5842a7ac4ebaa6b9fa8
Auberon López <auberon@adadev.org>
2023-02-17 12:38:46 -0800

    Support JavaScript tests.
    - Add option to specify using pip or yarn.
    
    - Update README with new usgae instructions.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # local-test-runner
-Copies a repository and runs pytests in the repo's virtual environment.
+Copies a repository and runs tests.
+
+For Python projects, use `pip` as the argument before the repo link. For JS projects, use `yarn`.
 
 Usage:
 ```
-./run_tests.sh github-repo-link
+./run_tests.sh pip|yarn github-repo-link
 ```
 
 If any environment variables are needed, simply export them before running the script.
--- run_tests.sh
+++ run_tests.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 set -e
 if [[ $# -eq 0 ]]; then
-  echo "Missing GitHub repo link"
-  echo "Usage: ./run_tests.sh github-repo-link"
+  echo "Missing arguments"
+  echo "Usage: ./run_tests.sh pip|yarn github-repo-link"
   exit 1
-fi 
+fi
+if [[ $1 != "pip" && $1 != "yarn" ]]; then
+  echo "Must either use pip or yarn"
+  echo "Usage: ./run_tests.sh pip|yarn github-repo-link"
+  exit 1
+fi
 cd repos
-USERNAME=$(echo $1 | rev | cut -d/ -f 2 | rev)
-REPO=$(echo $1 | rev | cut -d/ -f 1 | rev)
+USERNAME=$(echo $2 | rev | cut -d/ -f 2 | rev)
+REPO=$(echo $2 | rev | cut -d/ -f 1 | rev)
 mkdir -p $USERNAME
 cd $USERNAME
 rm -rf $REPO
-git clone $1
+git clone $2
 cd $REPO
-python3 -m venv venv
-. ./venv/bin/activate
-pip install -r requirements.txt
-pytest
+if [[ $1 == "pip" ]]; then
+  python3 -m venv venv
+  . ./venv/bin/activate
+  pip install -r requirements.txt
+  pytest
+elif [[ $1 == "yarn" ]]; then
+  yarn install
+  yarn test --watchAll=false
+else
+  echo "Supported options are pip or yarn"
+  echo "Usage: ./run_tests.sh pip|yarn github-repo-link"
+fi
